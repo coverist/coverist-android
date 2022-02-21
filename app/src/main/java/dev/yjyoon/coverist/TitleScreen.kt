26f5a094f21b58d0129518b24f03a01dd6a18e37
DESKTOP-EA4HSD4\yjyoon <yjyoon.dev@gmail.com>
2022-02-22 02:25:31 +0900

    [feat] TitleScreen

---
+++ app/src/main/java/dev/yjyoon/coverist/TitleScreen.kt
@@ -0,0 +1,48 @@
+package dev.yjyoon.coverist
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+
+@Composable
+fun TitleScreen(navController: NavController) {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colors.background
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceAround
+        ) {
+            Logo()
+            StartButton(navController = navController)
+        }
+    }
+}
+
+@Composable
+fun Logo() {
+    Text(text = "Coverist", style = MaterialTheme.typography.h3.copy(fontWeight = FontWeight.Bold))
+}
+
+@Composable
+fun StartButton(navController: NavController) {
+    Button(
+        onClick = { navController.navigate("book-info-input") },
+        contentPadding = PaddingValues(vertical = 18.dp, horizontal = 24.dp),
+        shape = MaterialTheme.shapes.medium
+    ) {
+        Text("새로운 표지 만들기", style = MaterialTheme.typography.h6)
+    }
+}
