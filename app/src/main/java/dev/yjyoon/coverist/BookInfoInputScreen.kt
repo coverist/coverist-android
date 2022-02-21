a179377e5567bb201c1720422a6fd9a66f641812
DESKTOP-EA4HSD4\yjyoon <yjyoon.dev@gmail.com>
2022-02-22 02:26:14 +0900

    [feat] Book info input screen draft

---
+++ app/src/main/java/dev/yjyoon/coverist/BookInfoInputScreen.kt
@@ -0,0 +1,9 @@
+package dev.yjyoon.coverist
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun BookInfoInputScreen() {
+    Text("BookInfoInput")
+}
