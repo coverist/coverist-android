87aaa5e8e214170b5e4eb6d6ea49c87aa5d971ac
DESKTOP-EA4HSD4\yjyoon <yjyoon.dev@gmail.com>
2022-03-07 07:42:52 +0900

    [fix] apply json serialized name

--- app/src/main/java/dev/yjyoon/coverist/data/remote/model/Book.kt
+++ app/src/main/java/dev/yjyoon/coverist/data/remote/model/Book.kt
@@ -1,10 +1,12 @@
 package dev.yjyoon.coverist.data.remote.model
 
+import com.google.gson.annotations.SerializedName
+
 data class Book(
     val title: String,
     val author: String,
     val genre: String,
-    val subGenre: String,
+    @SerializedName("sub_genre") val subGenre: String,
     val tags: List<String>,
     val publisher: String?
 )
--- app/src/main/java/dev/yjyoon/coverist/data/remote/model/Cover.kt
+++ app/src/main/java/dev/yjyoon/coverist/data/remote/model/Cover.kt
@@ -1,12 +1,14 @@
 package dev.yjyoon.coverist.data.remote.model
 
+import com.google.gson.annotations.SerializedName
+
 data class Cover(
-    val coverId: Long,
-    val bookId: Long,
+    @SerializedName("cover_id") val coverId: Long,
+    @SerializedName("book_id") val bookId: Long,
     val title: String,
     val author: String,
     val genre: String,
-    val subGenre: String,
+    @SerializedName("sub_genre") val subGenre: String,
     val tags: List<String>,
     val publisher: String?,
     val url: String
