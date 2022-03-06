b03caf429d51ea5baed929cdcd2f831ca8c793c7
DESKTOP-EA4HSD4\yjyoon <yjyoon.dev@gmail.com>
2022-03-07 06:40:46 +0900

    [ui] show cover screen

---
+++ app/src/main/java/dev/yjyoon/coverist/ui/showcover/ShowCoverScreen.kt
@@ -0,0 +1,127 @@
+package dev.yjyoon.coverist.ui.showcover
+
+import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import dev.yjyoon.coverist.ui.theme.CoveristTheme
+import dev.yjyoon.coverist.util.HorizontalImageCarousel
+
+@Composable
+fun ShowCoverScreen(
+    navController: NavController,
+    viewModel: ShowCoverViewModel
+) {
+
+    BackHandler {
+        navController.navigate("title") {
+            popUpTo("show-cover") { inclusive = true }
+        }
+    }
+
+    Scaffold(
+        backgroundColor = MaterialTheme.colors.primary,
+        contentColor = MaterialTheme.colors.onPrimary,
+        bottomBar = {
+            ConfirmButton(navController = navController)
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+        ) {
+            ShowCoverTitle()
+            ShowCoverContent()
+        }
+    }
+}
+
+@Composable
+fun ShowCoverTitle(modifier: Modifier = Modifier) {
+    Surface(
+        shape = MaterialTheme.shapes.medium,
+        color = Color.Black.copy(alpha = 0.12f),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(18.dp)
+    ) {
+        Text(
+            "새로운 표지가 탄생했어요!",
+            style = MaterialTheme.typography.h5.copy(fontWeight = FontWeight.Bold),
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(horizontal = 12.dp, vertical = 18.dp)
+        )
+    }
+}
+
+@Composable
+fun ConfirmButton(
+    navController: NavController,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(18.dp)
+    ) {
+        Button(
+            onClick = {
+                navController.navigate("title") {
+                    popUpTo("show-cover") { inclusive = true }
+                }
+            },
+            colors = ButtonDefaults.buttonColors(
+                backgroundColor = MaterialTheme.colors.onPrimary,
+                contentColor = MaterialTheme.colors.primary
+            ),
+            shape = MaterialTheme.shapes.medium,
+            contentPadding = PaddingValues(vertical = 12.dp, horizontal = 20.dp),
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("완료", style = MaterialTheme.typography.subtitle1)
+        }
+    }
+}
+
+@Composable
+fun ShowCoverContent(modifier: Modifier = Modifier) {
+    val list = listOf(
+        "", "", ""
+    )
+    HorizontalImageCarousel(
+        imageUrlList = list,
+        aspectRatio = 3 / 4f
+    )
+}
+
+@Composable
+fun CoverImage(
+    text: String
+) {
+    Surface(
+        color = Color.Yellow
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Text(text)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ShowCoverScreenPreview() {
+    CoveristTheme {
+    }
+}
---
+++ app/src/main/java/dev/yjyoon/coverist/util/HorizontalImageCarousel.kt
@@ -0,0 +1,76 @@
+package dev.yjyoon.coverist.util
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Card
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.util.lerp
+import coil.compose.AsyncImage
+import com.google.accompanist.pager.*
+import kotlin.math.absoluteValue
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+fun HorizontalImageCarousel(
+    imageUrlList: List<String>,
+    aspectRatio: Float,
+) {
+    Column(modifier = Modifier.fillMaxSize()) {
+        val pagerState = rememberPagerState()
+
+        HorizontalPager(
+            count = imageUrlList.size,
+            state = pagerState,
+            // Add 32.dp horizontal padding to 'center' the pages
+            contentPadding = PaddingValues(horizontal = 32.dp),
+            modifier = Modifier.weight(1f).fillMaxWidth()
+        ) { page ->
+            Card(
+                Modifier
+                    .graphicsLayer {
+                        // Calculate the absolute offset for the current page from the
+                        // scroll position. We use the absolute value which allows us to mirror
+                        // any effects for both directions
+                        val pageOffset = calculateCurrentOffsetForPage(page).absoluteValue
+
+                        // We animate the scaleX + scaleY, between 85% and 100%
+                        lerp(
+                            start = 0.85f,
+                            stop = 1f,
+                            fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                        ).also { scale ->
+                            scaleX = scale
+                            scaleY = scale
+                        }
+
+                        // We animate the alpha, between 50% and 100%
+                        alpha = lerp(
+                            start = 0.5f,
+                            stop = 1f,
+                            fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                        )
+                    }
+                    .aspectRatio(aspectRatio),
+                elevation = 7.dp
+            ) {
+                AsyncImage(
+                    model = imageUrlList[page],
+                    contentDescription = null,
+                    contentScale = ContentScale.FillWidth,
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+        }
+
+        HorizontalPagerIndicator(
+            pagerState = pagerState,
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(16.dp),
+        )
+    }
+}
