222f62a03d75ade56ae7084e903bb391183ffd4c
DESKTOP-EA4HSD4\yjyoon <yjyoon.dev@gmail.com>
2022-03-17 07:58:08 +0900

    [fix] set connection & read timeout

--- app/src/main/java/dev/yjyoon/coverist/di/DataSourceModule.kt
+++ app/src/main/java/dev/yjyoon/coverist/di/DataSourceModule.kt
@@ -6,8 +6,10 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import dev.yjyoon.coverist.data.remote.api.CoverService
 import dev.yjyoon.coverist.data.remote.api.GenreService
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Named
 import javax.inject.Singleton
 
@@ -23,19 +25,23 @@ class DataSourceModule {
     @Singleton
     @Provides
     fun provideRetrofit(@Named("BaseUrl") baseUrl: String): Retrofit {
+        val okHttpClient = OkHttpClient.Builder().readTimeout(30, TimeUnit.SECONDS)
+            .connectTimeout(30, TimeUnit.SECONDS).build()
+
         return Retrofit.Builder()
             .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
             .baseUrl(baseUrl)
             .build()
     }
 
     @Singleton
     @Provides
-    fun genreService(retrofit: Retrofit) : GenreService =
+    fun genreService(retrofit: Retrofit): GenreService =
         retrofit.create(GenreService::class.java)
 
     @Singleton
     @Provides
-    fun coverService(retrofit: Retrofit) : CoverService =
+    fun coverService(retrofit: Retrofit): CoverService =
         retrofit.create(CoverService::class.java)
 }
