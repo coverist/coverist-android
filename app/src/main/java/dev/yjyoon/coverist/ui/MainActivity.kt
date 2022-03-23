c905a5d432a8d615b476264dc62f759f3533e4f2
DESKTOP-EA4HSD4\yjyoon <yjyoon.dev@gmail.com>
2022-03-23 22:24:52 +0900

    [refactor] navigation

--- app/src/main/java/dev/yjyoon/coverist/ui/MainActivity.kt
+++ app/src/main/java/dev/yjyoon/coverist/ui/MainActivity.kt
@@ -3,19 +3,9 @@ package dev.yjyoon.coverist.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
-import dev.yjyoon.coverist.ui.bookshelf.BookShelfDetail
-import dev.yjyoon.coverist.ui.cover.generation.GenerateCoverScreen
 import dev.yjyoon.coverist.ui.theme.CoveristTheme
-import dev.yjyoon.coverist.ui.title.TitleScreen
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -24,39 +14,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             CoveristTheme {
                 val navController = rememberNavController()
-                CoveristNavHost(
-                    navController = navController
-                )
+                NavGraph(navController = navController)
             }
         }
     }
 }
 
-@Composable
-fun CoveristNavHost(
-    navController: NavHostController,
-    modifier: Modifier = Modifier
-) {
-    NavHost(
-        navController = navController,
-        startDestination = "title"
-    ) {
-        composable("title") {
-            TitleScreen(navController = navController)
-        }
-        composable("cover-generation") {
-            GenerateCoverScreen(navController = navController, viewModel = hiltViewModel())
-        }
-        composable("bookshelf-detail") {
-            BookShelfDetail()
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    CoveristTheme {
-        TitleScreen(navController = rememberNavController())
-    }
-}
---
+++ app/src/main/java/dev/yjyoon/coverist/ui/Navigation.kt
@@ -0,0 +1,39 @@
+package dev.yjyoon.coverist.ui
+
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import dev.yjyoon.coverist.ui.bookshelf.BookShelfDetail
+import dev.yjyoon.coverist.ui.bookshelf.BookShelfViewModel
+import dev.yjyoon.coverist.ui.cover.generation.GenerateCoverScreen
+import dev.yjyoon.coverist.ui.title.TitleScreen
+
+object NavDestination {
+    const val TITLE = "title"
+    const val COVER_GENERATION = "cover-generation"
+    const val BOOKSHELF_DETAIL = "bookshelf-detail"
+}
+
+@Composable
+fun NavGraph(navController: NavHostController) {
+    // Declare singleton view models here.
+    val bookShelfViewModel: BookShelfViewModel = hiltViewModel()
+
+    NavHost(
+        navController = navController,
+        startDestination = NavDestination.TITLE
+    ) {
+        composable(NavDestination.TITLE) {
+            TitleScreen(navController = navController, bookShelfViewModel = bookShelfViewModel)
+        }
+        composable(NavDestination.COVER_GENERATION) {
+            GenerateCoverScreen(navController = navController, viewModel = hiltViewModel())
+        }
+        composable(NavDestination.BOOKSHELF_DETAIL) {
+            BookShelfDetail(viewModel = bookShelfViewModel)
+        }
+    }
+}
+
