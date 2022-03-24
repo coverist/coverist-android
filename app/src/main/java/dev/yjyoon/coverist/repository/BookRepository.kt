f4e52239da5d75ba389ae3380a22d4287002a7b4
DESKTOP-EA4HSD4\yjyoon <yjyoon.dev@gmail.com>
2022-03-24 20:16:39 +0900

    [feat] add cover of existing book

--- app/src/main/java/dev/yjyoon/coverist/repository/BookRepository.kt
+++ app/src/main/java/dev/yjyoon/coverist/repository/BookRepository.kt
@@ -2,7 +2,7 @@ package dev.yjyoon.coverist.repository
 
 import dev.yjyoon.coverist.data.db.dao.BookDao
 import dev.yjyoon.coverist.data.db.entity.BookEntity
-import dev.yjyoon.coverist.ui.bookshelf.BookShelf
+import dev.yjyoon.coverist.ui.bookshelf.BookShelfState
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
@@ -12,7 +12,7 @@ interface BookRepository {
     suspend fun addBook(book: BookEntity)
     suspend fun updateBook(book: BookEntity)
     suspend fun deleteBook(book: BookEntity)
-    fun getAllBooks(): Flow<BookShelf>
+    fun getAllBooks(): Flow<BookShelfState>
 }
 
 class BookRepositoryImpl @Inject constructor(
@@ -30,11 +30,11 @@ class BookRepositoryImpl @Inject constructor(
         bookDao.deleteBook(book)
     }
 
-    override fun getAllBooks(): Flow<BookShelf> = flow {
-        emit(BookShelf.Loading)
+    override fun getAllBooks(): Flow<BookShelfState> = flow {
+        emit(BookShelfState.Loading)
         bookDao.getAllBooks().collect {
-            if (it.isEmpty()) emit(BookShelf.Empty)
-            else emit(BookShelf.Success(it))
+            if (it.isEmpty()) emit(BookShelfState.Empty)
+            else emit(BookShelfState.Success(it))
         }
-    }.catch { e -> emit(BookShelf.Error(e)) }
+    }.catch { e -> emit(BookShelfState.Error(e)) }
 }
