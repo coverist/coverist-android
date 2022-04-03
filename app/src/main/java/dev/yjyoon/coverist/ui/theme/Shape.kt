82d1cdd6127ec716b10f5650d7e133a2521d7639
DESKTOP-EA4HSD4\yjyoon <yjyoon.dev@gmail.com>
2022-04-03 23:17:36 +0900

    [ui] title drawer

--- app/src/main/java/dev/yjyoon/coverist/ui/theme/Shape.kt
+++ app/src/main/java/dev/yjyoon/coverist/ui/theme/Shape.kt
@@ -2,6 +2,12 @@ package dev.yjyoon.coverist.ui.theme
 
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Shapes
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Outline
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 
 val Shapes = Shapes(
@@ -9,3 +15,20 @@ val Shapes = Shapes(
     medium = RoundedCornerShape(24.dp),
     large = RoundedCornerShape(24.dp)
 )
+
+val TitleDrawerShape = object : Shape {
+    override fun createOutline(
+        size: Size,
+        layoutDirection: LayoutDirection,
+        density: Density
+    ): Outline {
+        return Outline.Rectangle(
+            Rect(
+                0f,
+                0f,
+                size.width * 0.9f,
+                size.height /* height */
+            )
+        )
+    }
+}
--- app/src/main/java/dev/yjyoon/coverist/ui/title/TitleScreen.kt
+++ app/src/main/java/dev/yjyoon/coverist/ui/title/TitleScreen.kt
@@ -2,28 +2,31 @@ package dev.yjyoon.coverist.ui.title
 
 import android.content.res.Configuration
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Info
 import androidx.compose.material.icons.rounded.Menu
+import androidx.compose.material.icons.rounded.Web
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.blur
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shadow
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import coil.compose.AsyncImage
+import dev.yjyoon.coverist.BuildConfig
 import dev.yjyoon.coverist.ui.bookshelf.BookShelfRow
 import dev.yjyoon.coverist.ui.bookshelf.BookshelfViewModel
+import dev.yjyoon.coverist.ui.theme.TitleDrawerShape
 import kotlinx.coroutines.launch
 
 @Composable
@@ -37,12 +40,8 @@ fun TitleScreen(
 
     Scaffold(
         scaffoldState = scaffoldState,
-        drawerContent = {
-        },
-        drawerShape = MaterialTheme.shapes.medium.copy(
-            topStart = CornerSize(0.dp),
-            bottomStart = CornerSize(0.dp)
-        ),
+        drawerContent = { TitleDrawerContent() },
+        drawerShape = TitleDrawerShape,
         bottomBar = {
             StartButton(
                 onClick = {
@@ -84,6 +83,7 @@ fun TitleGraphic(
             .height(configuration.screenWidthDp.dp)
             .background(color = MaterialTheme.colors.primary)
     ) {
+        /*
         AsyncImage(
             "https://mblogthumb-phinf.pstatic.net/MjAyMDA3MTRfNzAg/MDAxNTk0Njk2ODE2MTIy.pv4Ij7GFJwXLBKhwL2Jjcj59WdDc5hfdmzdVjUycDHkg.x51bDgp1jKX3SuQsWoHdWIu9OcaBOrpy5gtWmr9niWAg.PNG.zencstory/SE-b3c3d58a-e05d-4285-a9bb-8a9c67e07643.png?type=w800",
             contentDescription = null,
@@ -92,7 +92,8 @@ fun TitleGraphic(
                 .fillMaxWidth()
                 .align(Alignment.Center)
                 .blur(4.dp)
-        )
+
+        )*/
         Text(
             "내 작품의 새로운 얼굴,\n이젠 쉽고 간편하게",
             style = MaterialTheme.typography.h5.copy(
@@ -121,10 +122,65 @@ fun TitleGraphic(
             )
         }
     }
-
-
 }
 
+@Composable
+fun TitleDrawerContent() {
+    Column {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(color = MaterialTheme.colors.primary)
+                .padding(48.dp),
+            contentAlignment = Alignment.Center
+        ) {
+        }
+        Divider()
+        LazyColumn {
+            item {
+                Spacer(Modifier.height(16.dp))
+            }
+            item {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clickable { }
+                        .padding(vertical = 20.dp, horizontal = 24.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(Icons.Rounded.Web, contentDescription = null, tint = Color.Gray)
+                    Spacer(Modifier.width(16.dp))
+                    Text("웹페이지 바로가기")
+                }
+            }
+            item {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clickable { }
+                        .padding(vertical = 20.dp, horizontal = 24.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(Icons.Rounded.Info, contentDescription = null, tint = Color.Gray)
+                    Spacer(Modifier.width(16.dp))
+                    Text("오픈소스 라이센스")
+                }
+            }
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxHeight()
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            Text(
+                "버전 ${BuildConfig.VERSION_NAME}",
+                modifier = Modifier.align(Alignment.BottomStart),
+                style = TextStyle(color = Color.Gray, fontWeight = FontWeight.Bold)
+            )
+        }
+    }
+}
 
 @Composable
 fun StartButton(
@@ -133,7 +189,7 @@ fun StartButton(
 ) {
     Button(
         onClick = onClick,
-        contentPadding = PaddingValues(vertical = 12.dp, horizontal = 20.dp),
+        contentPadding = PaddingValues(vertical = 14.dp, horizontal = 20.dp),
         modifier = Modifier
             .padding(18.dp)
             .fillMaxWidth()
