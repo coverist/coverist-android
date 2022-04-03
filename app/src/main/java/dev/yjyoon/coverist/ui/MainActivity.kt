c6bbcbdc82c0bca7c5e6526eb67ae0e98fe1928d
DESKTOP-EA4HSD4\yjyoon <yjyoon.dev@gmail.com>
2022-04-04 03:04:51 +0900

    [feat] splash screen (not work yet)

--- app/src/main/java/dev/yjyoon/coverist/ui/MainActivity.kt
+++ app/src/main/java/dev/yjyoon/coverist/ui/MainActivity.kt
@@ -3,6 +3,7 @@ package dev.yjyoon.coverist.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
 import dev.yjyoon.coverist.ui.theme.CoveristTheme
@@ -11,6 +12,7 @@ import dev.yjyoon.coverist.ui.theme.CoveristTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             CoveristTheme {
                 val navController = rememberNavController()
